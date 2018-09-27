01aad87f300b55273cea392034dd4bd88848b858
vlevko <vlevko@student.unit.ua>
2018-09-27 17:23:36 +0300

    Palindrome.py v2

--- Palindrome.py
+++ Palindrome.py
@@ -4,14 +4,6 @@ class Palindrome:
 
     @staticmethod
     def is_palindrome(word):
-        idx = -1
-        mid = len(word) / 2
-        for character in word:
-            if character.lower() != word[idx].lower():
-                return False
-            idx -= 1
-            if -idx > mid:
-                break
-        return True
+        return word.lower() == word[::-1].lower()
 
 print(Palindrome.is_palindrome('Deleveled'))
