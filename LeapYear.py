d3fe8357c67cf59b9375ac1f647ec5587a197289
vlevko <vlevko@student.unit.ua>
2018-10-04 01:06:02 +0300

    LeapYear.py v2

--- LeapYear.py
+++ LeapYear.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+import calendar
+
 class LeapYear:
 
     @staticmethod
     def is_leap_year(year):
-        return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+        return calendar.isleap(year)
 
 print(LeapYear.is_leap_year(2000))
